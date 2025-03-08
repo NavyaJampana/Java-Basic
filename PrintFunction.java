4c97ce235ae2eebb18c1c577e6d783ede680c4ae
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 16:26:54 +0530

    Add files via upload

---
+++ PrintFunction.java
@@ -0,0 +1,12 @@
+package JavaBasics;
+public class PrintFunction {
+    // Function to print name
+    static void printName() {
+        System.out.println("Alekya");
+    }
+
+    public static void main(String[] args) {
+        printName(); // Calling the function
+    }
+}
+
