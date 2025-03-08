f880e178f02e4f93516fbf8ba457b3425855272e
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 16:26:00 +0530

    Add files via upload

---
+++ DataTypesExample.java
@@ -0,0 +1,17 @@
+package JavaBasics;
+public class DataTypesExample {
+    public static void main(String[] args) {
+        int num = 25;
+        boolean isTrue = true;
+        char letter = 'A';
+        float floatNum = 5.75f;
+        double doubleNum = 19.99;
+
+        System.out.println("Integer: " + num);
+        System.out.println("Boolean: " + isTrue);
+        System.out.println("Character: " + letter);
+        System.out.println("Float: " + floatNum);
+        System.out.println("Double: " + doubleNum);
+    }
+}
+
